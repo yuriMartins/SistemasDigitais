5135379113fbf3ba9deedadb6ac6f4736a9f668d
jhonedarts <jhonedarts@gmail.com>
2016-08-03 09:44:34 -0300

    Codigos Brutos
    Preciso que vcs refatorem um desses pra nosso processador, para que eu
    saiba como refatorar os cabeçarios. E então e refatoro o restante
    
    ps: o fibonacci deve ser recursivo

---
+++ CodigosDeTeste/FibonacciRecursive.asm
@@ -0,0 +1,96 @@
+﻿# kkamin8
+# MIPS fibonacci
+
+.data
+msg1: .asciiz "Enter n: "
+msg2: .asciiz "th Fibonacci is: "
+
+.text
+    # ask user for the input
+    li          $v0, 4
+    la          $a0, msg1
+    syscall
+    li          $v0, 5
+    syscall
+    move        $s1, $v0
+
+    # decrement $v0 since 1st fibonacci is f(0) = 0, 2nd is f(1) = 1 etc...
+    addi        $v0, $v0, -1
+
+    addi        $sp, $sp, -12
+    sw          $v0, 0($sp)
+    sw          $ra, 8($sp)
+    jal         fibonacci
+        
+    lw          $ra, 8($sp)
+    lw          $s0, 4($sp)
+    addi        $sp, $sp, 12
+
+    # print result
+    move        $a0, $s1
+    li          $v0, 1
+    syscall
+
+    la          $a0, msg2
+    li          $v0, 4
+    syscall
+
+    move        $a0, $s0
+    li          $v0, 1
+    syscall
+
+    li          $v0, 10
+    syscall
+
+fibonacci:
+    lw          $t0, 0($sp)
+    # if n > 46, return 0 and don't overflow 32-bit register
+    bge         $t0, 46, return0
+
+    # base case
+    ble         $t0, 0, return0
+    beq         $t0, 1, return1
+
+    # fibonacci(n - 1)
+    addi        $t0, $t0, -1
+    addi        $sp, $sp, -12
+    sw          $t0, 0($sp)
+    sw          $ra, 8($sp)
+    jal         fibonacci
+    # reload (n - 1)
+    lw          $t0, 0($sp)
+    # get return value from fibonacci(n - 1)
+    lw          $t1, 4($sp)
+    lw          $ra, 8($sp)
+    addi        $sp, $sp, 12
+    # save the return value from fibonacci(n - 1) on the stack
+    addi        $sp, $sp, -4
+    sw          $t1, 0($sp)
+
+    # fibonacci(n - 2)
+    addi        $t0, $t0, -1
+    addi        $sp, $sp, -12
+    sw          $t0, 0($sp)
+    sw          $ra, 8($sp)
+    jal         fibonacci
+    # get return value from fibonacci(n - 2)
+    lw          $t2, 4($sp)
+    lw          $ra, 8($sp)
+    addi        $sp, $sp, 12
+    lw          $t1, 0($sp)
+    addi        $sp, $sp, 4
+
+    # $t3 = fibonacci(n - 1) + fibonacci(n - 2)
+    add         $t3, $t1, $t2
+    sw          $t3, 4($sp)
+
+    jr          $ra 
+
+return0:
+    sw          $0, 4($sp)
+    jr          $ra  
+
+return1:
+    li          $t0, 1
+    sw          $t0, 4($sp)
+    jr          $ra
---
+++ CodigosDeTeste/Potencia.asm
@@ -0,0 +1,26 @@
+﻿Potencia
+
+.data  r: .word 0.textmain:
+
+li $v0,5   #leitura de x
+syscall
+add $s0,$v0,0
+li $v0,5   #leitura de y
+syscall
+add $s1,$v0,0
+add $s2,$s0,0
+li $t0,1
+
+while:  
+	beq $t0,$s1,fim  
+	mul $s2,$s2,$s0  
+	add $t0,$t0,1  
+j while
+
+fim:
+	sw $s2,r
+	li $v0,1
+	lw $a0,r
+	syscall
+	li $v0,10
+	syscall
