da5ce2c7c2503038fa5089992cc828488b65a35e
Leocampos1 <leocamposouza@gmail.com>
2016-08-08 21:51:25 -0300

    Update bubllesort.asm

--- CodigosDeTeste/bubllesort.asm
+++ CodigosDeTeste/bubllesort.asm
@@ -1,21 +1,21 @@
 .module bublesort
 .data
 .pseg
-	move $s0,$gp			#get the intial point to save array 
+	move $s0,$gp			#pega o ponteiro inicial e salva no array. 
 	addi $t0,1			# $t0 = 1
 	add $t1,$zero,$zero		# 
 	add $t2,$zero,$zero		# 
 	add $t3,$zero,$zero		# 
 	add $t6,$zero,$zero        
 	add $t4,$zero,$zero        
-	sub $t7,$zero,1			# terminate        
-	add $s1,$s0,$zero	# copy the pointer to array in $s1
+	sub $t7,$zero,1			#         
+	add $s1,$s0,$zero		# copia o ponteiro do array em $s1
 entervalues:
-	li $v0,5		# get the value in v0 
-	beq $v0,$t7,bubblesort # end of string run to bubblesort
-	sb $v0,0($s1)	# put the value at the position pointed by $s1
-	addi $s1,1		# move the $s1 pointer by one
-	add $t5,$s1,$zero # $t5 stores the end value
+	li $v0,5			# atribui um inteiro em v0 
+	beq $v0,$t7,bubblesort 		# end of string run to bubblesort
+	sb $v0,0($s1)			# put the value at the position pointed by $s1
+	addi $s1,1			# move the $s1 pointer by one
+	add $t5,$s1,$zer		# $t5 stores the end value
 	j entervalues
 bubblesort:
 	add $t4,$s0,$zero
@@ -35,11 +35,5 @@ loopinterno:
 proximo:
 	addi $s2,1		#
 	bne $s2,$s1,loopinterno #
-	
-imprime:
-		
-	addi $t4,1
-	bne $t4,$t5,imprime
-jal bubblesort	
-ending:
+
 .endseg	 
